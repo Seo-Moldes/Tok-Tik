8ac8f617153ee45bf77066e001678cb64236a1b2
Seo-Moldes <seomoldes@gmail.com>
2024-12-09 18:57:01 +0000

    full functional app with bugs

--- toktik/lib/src/widgets/fullscreen_player.dart
+++ toktik/lib/src/widgets/fullscreen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/src/widgets/video_background.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -40,8 +41,57 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
 
     return FutureBuilder(future: controller.initialize(), builder: (context, snapshot) {
 
+      if(snapshot.connectionState != ConnectionState.done) {
+
       return const Center( child: CircularProgressIndicator(strokeWidth: 2, color: Colors.white));
+        
+      }
+
+      return GestureDetector(
+        onTap: () {
+          if(controller.value.isPlaying) {
+            controller.pause();
+            return;
+          }
+          controller.play();
+        } ,
+        child: AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: Stack(
+            children: [
+              VideoPlayer(controller),
+              const VideoBackground(stops: [0.8, 1.0],),
+              Positioned(
+                bottom: 50,
+                left: 20,
+                child: VideoCaption(caption: widget.caption) )
+            ],
+          )
+          
+          ),
+      );
+
     },
+    );
+  }
+}
+
+class VideoCaption extends StatelessWidget {
+
+  final String caption;
+  
+
+  const VideoCaption({super.key, required this.caption});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(caption, maxLines: 2,),
+
     );
   }
 }
